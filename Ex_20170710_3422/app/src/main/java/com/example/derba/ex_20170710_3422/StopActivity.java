7db8c535dec404695d1cef2de2a67a08f2a0f281
ITJEONG-NAN-JJANG <derbana1027@gmail.com>
2017-07-10 16:30:22 +0900

    ad
    ad

--- Ex_20170710_3422/app/src/main/java/com/example/derba/ex_20170710_3422/StopActivity.java
+++ Ex_20170710_3422/app/src/main/java/com/example/derba/ex_20170710_3422/StopActivity.java
@@ -1,13 +1,54 @@
 package com.example.derba.ex_20170710_3422;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class StopActivity extends AppCompatActivity {
+public class StopActivity extends AppCompatActivity
+{
+    MyIntentService mService;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop);
+
+        Intent it = new Intent(this, MyIntentService.class);
+        bindService(it, conn, Context.BIND_ABOVE_CLIENT);
     }
+
+    public void onMusicStop(View v)
+    {
+        Intent it = new Intent(this, MyIntentService.class);
+        stopService(it);
+    }
+
+    public void onGetRunTime(View v)
+    {
+        int a = mService.mp.getCurrentPosition();
+        System.out.println("time : "+a);
+    }
+
+    ServiceConnection conn = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder)
+        {
+
+            MyIntentService.MusicBinder mb = (MyIntentService.MusicBinder)iBinder;
+
+            mService = mb.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName)
+        {
+        }
+    };
 }
